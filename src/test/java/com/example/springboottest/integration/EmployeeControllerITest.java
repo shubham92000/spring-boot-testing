d73dd3ba50573e6603f4441a7f6f053f761ace20
shubham92000 <sbhsingh616@gmail.com>
2023-04-27 20:54:07 +0530

    IT

---
+++ src/test/java/com/example/springboottest/integration/EmployeeControllerITest.java
@@ -0,0 +1,60 @@
+package com.example.springboottest.integration;
+
+import com.example.springboottest.model.Employee;
+import com.example.springboottest.repository.EmployeeRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class EmployeeControllerITest {
+    // call the rest api
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setup(){
+        employeeRepository.deleteAll();
+    }
+
+    @Test
+    public void givenEmpObj_whenCreateEmp_thenReturnSavedEmp() throws Exception {
+        Employee employee = Employee.builder()
+                .firstName("shubham")
+                .lastName("sneha")
+                .email("s@s.com")
+                .build();
+
+        // when
+        ResultActions response = mockMvc.perform(post("/api/employees")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(employee))
+        );
+
+        // then
+        response.andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName", CoreMatchers.is(employee.getFirstName())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName", CoreMatchers.is(employee.getLastName())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email", CoreMatchers.is(employee.getEmail())))
+                .andDo(MockMvcResultHandlers.print())
+        ;
+    }
+}
